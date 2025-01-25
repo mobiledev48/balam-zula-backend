86c1f9325c592a4f498bfbffdd8b7cfccd572daf
mobileappdev48 <apputilitystudio@gmail.com>
2025-01-25 16:09:35 +0530

    Changes : auth Remove From Wp Number !

--- routes/watsappNumber.js
+++ routes/watsappNumber.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var router = express.Router();
 var watsappNumberController = require('../controllers/watsappNumber');
-const authMiddleware = require('../middleware/authMiddleware');
+// const authMiddleware = require('../middleware/authMiddleware');
 
 
-router.post('/add', authMiddleware, watsappNumberController.addWatsappNumber);
+router.post('/add', watsappNumberController.addWatsappNumber);
 
-router.get('/', authMiddleware, watsappNumberController.getWatsappNumber);
+router.get('/', watsappNumberController.getWatsappNumber);
 
-router.put('/:updateId', authMiddleware, watsappNumberController.updateWatsappNumber);
+router.put('/:updateId', watsappNumberController.updateWatsappNumber);
 
-router.delete('/:deleteId', authMiddleware, watsappNumberController.deleteWatsappNumber);
+router.delete('/:deleteId', watsappNumberController.deleteWatsappNumber);
+
+
+// router.post('/add', authMiddleware, watsappNumberController.addWatsappNumber);
+
+// router.get('/', authMiddleware, watsappNumberController.getWatsappNumber);
+
+// router.put('/:updateId', authMiddleware, watsappNumberController.updateWatsappNumber);
+
+// router.delete('/:deleteId', authMiddleware, watsappNumberController.deleteWatsappNumber);
 
 
 module.exports = router;
